9c89e1c5b5807285f9113246a78e78d7fe619f38
DanAzo <daniella.azouer@tuni.fi>
2023-06-22 04:49:39 +0300

    Add: Camera follow

---
+++ Leftovers_2023/Assets/Code/Units/CameraFollow.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leftovers_2DPlatformer
+{
+    public class CameraFollow : MonoBehaviour
+    {
+		private PlayerUnit player;
+
+		[SerializeField]
+		private float smoothTime;
+
+		private Vector3 velocity;
+		private Vector3 offset;
+
+		private void Start()
+		{
+			player = FindObjectOfType<PlayerUnit>();
+
+			Camera camera = GetComponent<Camera>();
+			if (camera != null)
+			{
+				offset = transform.position - player.transform.position;
+			}
+
+			offset = new Vector3(0, 1, -10);
+		}
+
+
+		// Update is called once per frame
+		void Update()
+		{
+			
+			if (player == null) return;
+
+			Vector3 targetPosition = player.transform.position + offset;
+
+			transform.position = Vector3.SmoothDamp(transform.position, targetPosition,
+				ref velocity, smoothTime * Time.deltaTime);
+
+			
+		}
+	}
+}
